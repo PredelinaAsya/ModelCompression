b46f57575cf02bcba42518b220a672d83a513c6e
Pronomuos <WizardPronomuos@yandex.ru>
2023-10-02 09:01:09 +0300

    Update: add mlflow tracking in hw1

--- hw1/base_metrics.py
+++ hw1/base_metrics.py
@@ -1,8 +1,17 @@
+import mlflow
 from ultralytics import YOLO
 
+from model_metrics import get_model_size, get_yolo_metrics
+
 # Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
-# Use the model
-metrics = model.val(data="coco128.yaml")  # evaluate model performance on the validation set
+with mlflow.start_run(run_name="initial") as run:
+    # Use the model
+    mlflow.log_param("compression_type", "none")
+    mlflow.log_metric("model_size", get_model_size(model))
+
+    metrics = model.val(data="coco128.yaml")  # evaluate model performance on the validation set
+    metrics = get_yolo_metrics(metrics)
+    for key, val in metrics.items():
+        mlflow.log_metric(key, val)
